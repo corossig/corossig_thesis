e97beb20687475ff7645279f200074faeb068221
Corentin Rossignon <corentin.rossignon@total.com>
2015-01-19 14:29:58 +0100

    Correction de fautes d'orthographes

--- These/img/plot.sh
+++ These/img/plot.sh
@@ -2,14 +2,28 @@
 
 for i in *.plot
 do
-    NB_COL=$(head -n 3 "${i}" | tail -n 1 | wc -w)
-    gnuplot -p << EOF
+    dest="${i%.*}.svg"
+    if [ ! -e "$dest" ] || [ "$dest" -ot "$i" ] || [ $# -gt 0 ]
+    then
+        NB_COL=$(head -n 3 "${i}" | tail -n 1 | wc -w)
+        YRANGE=12
+        case "$i" in
+            *_facto_*)
+                YRANGE=10
+                ;;
+            *_trsv_*)
+                YRANGE=3
+                ;;
+        esac
+        
+        gnuplot -p << EOF
 set xlabel "Nombre de threads"
 set ylabel "Speedup"
 set key left top noreverse width 2
-set yrange [0:10]
+set yrange [0:$YRANGE]
 set terminal svg size 700,500 fname 'Verdana' fsize 16
-set output "${i%.*}.svg"
+set output "$dest"
 plot for [i=2:${NB_COL}] "${i}" u 1:i title column(i) w lp
 EOF
+    fi
 done
