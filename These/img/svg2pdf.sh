9ca3f65498d30b0a4702819a5c59bcd20a846982
Corentin Rossignon <corossig@gmail.com>
2015-03-29 23:12:12 +0200

    Modif

--- These/img/svg2pdf.sh
+++ These/img/svg2pdf.sh
@@ -2,10 +2,21 @@
 
 for src in *.svg
 do
-    dest="${src%%.*}.pdf"
+    case $src in
+        r_*)
+            dest="${src#r_}"
+            dest="${dest%%.*}.png"
+            options="-d 180 -e"
+            ;;
+        *)
+            dest="${src%%.*}.pdf"
+            options="-A"
+            ;;
+    esac
+    
     if [ ! -e "$dest" ] || [ "$dest" -ot "$src" ] || [ $# -gt 0 ]
     then
         echo processing $src to $dest
-        inkscape -f "$src" -A "$dest"
+        inkscape -f "$src" ${options} "$dest"
     fi
 done
